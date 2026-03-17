99264ddd9ced11a9226dfc8fb2ecf294e6608cfd
Stefan Schneider <schneider@hrz.uni-marburg.de>
2026-03-17 10:55:22 +0100

    clean project files

--- cibuild.test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cibuild_log_info "this is a custom script in an unlocked job container, be careful!"
-
-assert_log "testlog" "/bin/sh" "-c" "echo 'testlog'; sleep infinity" 
-assert_response "Welcome to nginx" 80 "keep"
